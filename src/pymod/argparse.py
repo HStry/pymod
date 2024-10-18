bb368efad5724d47a91cf922caaabbcc27262a1c
Hans Strijker <45356184+HStry@users.noreply.github.com>
2024-10-18 14:20:31 +0200

    argparse helpformatting, exit_on_error patches

---
+++ src/pymod/argparse.py
@@ -0,0 +1,36 @@
+"""argparse patches"""
+
+__all__ = ['argparse']
+
+import argparse
+
+def _exit(self: argparse.ArgumentParser,
+          status: int = 0,
+          message: None | str = None) -> None:
+    if message:
+        self._print_message(message, argparse._sys.stderr)
+    if status and self.exit_on_error:
+        argparse._sys.exit(status)
+
+def _fmt_invoc(self: argparse.HelpFormatter,
+               action: argparse.Action) -> str:
+    if not action.option_strings:
+        default = self._get_default_metavar_for_positional(action)
+        metavar, = self._metavar_formatter(action, default)(1)
+        return metavar
+
+    else:
+        # if the Optional doesn't take a value, format is:
+        #    -s, --long
+        # if it does, the format is:
+        #    -s, --long ARGS
+        invoc = ', '.join(action.option_strings)
+        
+        if action.nargs == 0:
+            return invoc
+        
+        mvar = self._get_default_metavar_for_optional(action)
+        return f"{invoc} {self._format_args(action, mvar)}"
+
+argparse.ArgumentParser.exit = _exit
+argparse.HelpFormatter._format_action_invocation = _fmt_invoc
