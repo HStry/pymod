02f4c404709ac5d328093c8fb8a7c58c798b2994
Hans Strijker <45356184+HStry@users.noreply.github.com>
2024-10-23 11:14:54 +0200

    Added 'sources' function

--- src/pymod/_builtins.py
+++ src/pymod/_builtins.py
@@ -1,10 +1,11 @@
 "patches to builtin objects"
 
-__all__ = ['UNSET', 'isinstance']
+__all__ = ['UNSET', 'isinstance', 'sources']
 
 from collections.abc import Collection
 from typing import Optional, Union, Any
 from builtins import isinstance as _isinstance
+import inspect
 
 class UNSET(object):
     def __new__(cls):
@@ -30,3 +31,20 @@ def isinstance(obj: Any, types: T, antitypes: UT = UNSET):
         return _isinstance(obj, types)
     return (_isinstance(obj, types)
             and not _isinstance(obj, antitypes))
+
+def sources(*objects, **kwobjects):
+    "Prints the sources of provided objects."
+    keylength = max(0, len(str(len(objects))), *(len(str(k)) for k in kwobjects))
+    iobjects = ((f"{i:>{keylength}d}", o) for (i, o) in enumerate(objects))
+    
+    for key, obj in *iobjects, *kwobjects.items():
+        try:
+            name = f"{key} ({obj.__name__})"
+        except AttributeError:
+            name = f"{key}"
+        
+        print(f"# {name}:")
+        try:
+            print(inspect.getsource(obj))
+        except (TypeError, ValueError):
+            print("#    <unable to retrieve source>")
